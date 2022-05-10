f40db5b564abc638c6aabe704c69276fc81aa7c6
PLong148 <105147738+PLong148@users.noreply.github.com>
2022-05-10 18:35:10 +0700

    Add files via upload

---
+++ RestAPI/taco_cloud_API/src/main/java/tacos/Ingredient.java
@@ -0,0 +1,30 @@
+package tacos;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Id;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Data
+@RequiredArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
+@Entity
+public class Ingredient {
+	private final String id;
+	private final String name;
+	@Enumerated(EnumType.STRING)
+	private final Type type;
+
+	public static enum Type {
+		WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
+	}
+}
